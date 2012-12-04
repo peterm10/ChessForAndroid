aab3373ec2a24449e1816926cf0f10f6ca05f19e
Piotr Medrala <medralap22@gmail.com>
2012-12-04 17:29:06 +0100

    unit-test

--- TestChessAndroid/src/com/chess/chessandroid/test/BoardAttackTest.java
+++ TestChessAndroid/src/com/chess/chessandroid/test/BoardAttackTest.java
@@ -54,16 +54,46 @@ public class BoardAttackTest extends ActivityInstrumentationTestCase2<MainActivi
 	       
 	    }
 	public void testColor(){
-		assertEquals("Sprawdzanie czy wyœwietla siê kolor planszy", board.getColor(1));
+		assertEquals("Sprawdzanie czy wyœwietla siê kolor planszy",1, board.getColor(1));
 	}
 	
 	public void testPiece(){
-		assertEquals("Sprawdzanie czy wyœwietla siê szachownica", board.getPiece(1));
+		assertEquals("Sprawdzanie czy wyœwietla siê szachownica",1, board.getPiece(1));
 	}
 	public void testIsCheck(){
 		assertTrue("Sprawdzanie czy pionek jest w szachu", board.inCheck(1));
 	}
-	public void testAttack(){
-		assertTrue("Sprawdzanie ruchu komputera pionek zbija pionka z bia³ego pola", board.attack(3,1));
+	public void testAttack1(){
+		assertTrue("Sprawdzanie ataku na wierze na ciemnym polu", board.attack(A1,1));
+	}
+	public void testAttack2(){
+		assertTrue("Sprawdzanie ataku na skoczka na bia³ym polu", board.attack(B1,1));
+	}
+	public void testAttack3(){
+		assertTrue("Sprawdzanie ataku na gonca na ciemnym polu", board.attack(C1,1));
+	}
+	public void testAttack4(){
+		assertTrue("Sprawdzanie ataku na królowa na bia³ym polu", board.attack(D1,1));
+	}
+	public void testAttack5(){
+		assertTrue("Sprawdzanie ataku na króla na ciemnym polu", board.attack(E1,1));
+	}
+	public void testIsCheck6(){
+		assertTrue("Sprawdzanie czy pionek jest w szachu", board.inCheck(0));
+	}
+	public void testAttack7(){
+		assertTrue("Sprawdzanie ataku na wierze na bia³ym polu", board.attack(F1,0));
+	}
+	public void testAttack8(){
+		assertTrue("Sprawdzanie ataku na skoczka na bia³ym polu", board.attack(G1,0));
+	}
+	public void testAttack9(){
+		assertTrue("Sprawdzanie ataku na gonca na bia³ym polu", board.attack(H1,0));
+	}
+	public void testAttack10(){
+		assertTrue("Sprawdzanie ataku na królowa na bia³ym polu", board.attack(D1,0));
+	}
+	public void testAttack11(){
+		assertTrue("Sprawdzanie ataku na króla na bia³ym polu", board.attack(E1,0));
 	}
 }
