6cd369119431ed43c8b5b32459bcf034c5a6e0fb
Piotr Medrala <medralap22@gmail.com>
2012-12-18 15:15:30 +0100

    Add change all new code

---
+++ ChessAndroid/testy/com/chess/chessandroid/BoardTest.java
@@ -0,0 +1,248 @@
+/**
+ * 
+ */
+package com.chess.chessandroid;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Piotr
+ *
+ */
+public class BoardTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#Board()}.
+	 */
+	@Test
+	public final void testBoard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#getColor(int, int)}.
+	 */
+	@Test
+	public final void testGetColorIntInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#getColor(int)}.
+	 */
+	@Test
+	public final void testGetColorInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#getPiece(int, int)}.
+	 */
+	@Test
+	public final void testGetPieceIntInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#getPiece(int)}.
+	 */
+	@Test
+	public final void testGetPieceInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#inCheck(int)}.
+	 */
+	@Test
+	public final void testInCheck() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#attack(int, int)}.
+	 */
+	@Test
+	public final void testAttack() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#gen()}.
+	 */
+	@Test
+	public final void testGen() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#genCaps()}.
+	 */
+	@Test
+	public final void testGenCaps() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#genPush(java.util.Collection, int, int, int)}.
+	 */
+	@Test
+	public final void testGenPush() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#genPromote(java.util.Collection, int, int, int)}.
+	 */
+	@Test
+	public final void testGenPromote() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#makeMove(com.chess.chessandroid.Move)}.
+	 */
+	@Test
+	public final void testMakeMove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#takeBack()}.
+	 */
+	@Test
+	public final void testTakeBack() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#reps()}.
+	 */
+	@Test
+	public final void testReps() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#eval()}.
+	 */
+	@Test
+	public final void testEval() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#evalLightPawn(int)}.
+	 */
+	@Test
+	public final void testEvalLightPawn() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#evalDarkPawn(int)}.
+	 */
+	@Test
+	public final void testEvalDarkPawn() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#evalLightKing(int)}.
+	 */
+	@Test
+	public final void testEvalLightKing() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#evalLkp(int)}.
+	 */
+	@Test
+	public final void testEvalLkp() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#evalDarkKing(int)}.
+	 */
+	@Test
+	public final void testEvalDarkKing() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#evalDkp(int)}.
+	 */
+	@Test
+	public final void testEvalDkp() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#COL(int)}.
+	 */
+	@Test
+	public final void testCOL() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link com.chess.chessandroid.Board#ROW(int)}.
+	 */
+	@Test
+	public final void testROW() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
